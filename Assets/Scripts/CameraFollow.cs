4f834eed64037c0debe28958c7aff5572dcdbb7c
I-H <teemu@rautasydan.org>
2017-12-02 12:06:08 +0200

    Initial commit, movement and camera following.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform Target;
+    public bool FocusOnTarget = false;
+    Vector2 _focusPoint = Vector2.zero;
+    Vector2 _camTarget;
+    Vector2 _diff;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 tgtPos = Target.position;
+        Vector2 posNow = transform.position;
+
+        if (!FocusOnTarget)
+        {
+            _camTarget.x = (_focusPoint.x + tgtPos.x) / 2;
+            _camTarget.y = (_focusPoint.y + tgtPos.y) / 2;
+        } else
+        {
+            _focusPoint = tgtPos;
+            _camTarget = tgtPos;
+        }
+
+        _diff = posNow - _camTarget;
+
+        transform.position = Vector3.MoveTowards(posNow, _camTarget, (1+_diff.magnitude)/10) + -Vector3.forward;
+
+
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    [SerializeField]
+    private float _moveSpeed = 2f;
+    private Vector2 _targetPosition;
+    private Vector2 _distance;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*
+         * Input
+         */
+        if(Input.touchCount > 0)
+        {
+            ReadInput(Input.touches);
+        }
+
+        /*
+         * Moving towards target
+         */
+        Vector2 posNow = transform.position;
+        if(posNow != _targetPosition)
+        {
+            _distance = posNow - _targetPosition;
+            transform.position = Vector2.MoveTowards(posNow, _targetPosition, _moveSpeed*(Random.value/2+0.5f)/20);
+        } else
+        {
+        }
+	}
+
+    void ReadInput(Touch[] touches)
+    {
+        // Main touch
+        Touch t = touches[0];
+        _targetPosition = Camera.main.ScreenToWorldPoint(t.position);
+
+
+        // Other touches
+        if (touches.Length > 1)
+        {
+            for (int i = 0; i < touches.Length; i++)
+            {
+
+            }
+        }
+    }
+}
